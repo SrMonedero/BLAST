d138acc408d065268d63d6446a56a42f85fd15b9
Quique Alvarez <quique.alvarez@wallapop.com>
2018-04-05 19:16:44 +0200

    Change color on click

--- Assets/Scripts/CellBehaviour.cs
+++ Assets/Scripts/CellBehaviour.cs
@@ -6,11 +6,19 @@ public class CellBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetMouseButton(0))
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+    }
 }
