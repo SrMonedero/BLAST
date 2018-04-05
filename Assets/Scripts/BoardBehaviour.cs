732132f3e357e48c89bdae5609f74d6875415bc8
Quique Alvarez <quique.alvarez@wallapop.com>
2018-04-05 20:23:27 +0200

    Generate board

--- Assets/Scripts/BoardBehaviour.cs
+++ Assets/Scripts/BoardBehaviour.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardBehaviour : MonoBehaviour {
+    public float separation;
+    public int columns;
+    public int rows;
+    private GameObject[,] cells;
 
 	// Use this for initialization
 	void Start ()
     {
-        GameObject board = Instantiate(Resources.Load("Prefabs/Cell"), Vector3.zero, Quaternion.identity) as GameObject;
+        int midColumn = columns / 2;
+        int midRow = rows / 2;
+        cells = new GameObject[columns, rows];
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++)
+            {
+                int columnDistance = i - midColumn;
+                int rowDistance = j - midRow;
+                float xPos = columnDistance + columnDistance * separation + 0.5f + separation/2;
+                float yPos = rowDistance + rowDistance * separation + 0.5f + separation / 2;
+                cells[i,j] = Instantiate(Resources.Load("Prefabs/Cell"), new Vector3(xPos, yPos, 0), Quaternion.identity) as GameObject;
+            }
+        }
     }
 	
 	// Update is called once per frame
